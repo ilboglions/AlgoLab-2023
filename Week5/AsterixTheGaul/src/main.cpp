91c6409806e25b16b1a24dc1069e07b5128a78b6
Matteo Boglioni <boglioni.matteo@gmail.com>
2024-01-28 07:12:04 +0100

    Upgraded Asterix the Gaul

--- Week5/AsterixTheGaul/src/main.cpp
+++ Week5/AsterixTheGaul/src/main.cpp
@@ -1,109 +1,104 @@
 #include <iostream>
 #include <vector>
-#include <map>
-#include <algorithm>
+#include <utility>
 #include <climits>
+#include <algorithm>
+using namespace std;
 
-using std::map;
-using std::vector;
-void solve();
-int findbest(const vector<long>&,const vector<long>& ,const vector<long>& ,const  int, const  int, const long&,const long&);
-bool isdoable(const vector<long>& , const vector<long> &, long, const long &, const long & );
+typedef pair<long,long> moves;
 
-int main(){
-    std::ios_base::sync_with_stdio(false);
-    int t;
-    std::cin>>t;
-    while(t--){
-        solve();
+bool binary_search( vector <moves> & s, long distance, long maxtime){
+  long left = 0; 
+  long right = s.size()-1;
+  while(left <= right){
+    long mid = (left+right)/2;
+    if(s[mid].first >= distance){
+      if(s[mid].second < maxtime)
+        return true;
+      right = mid-1;
     }
+    else
+      left = mid+1;
+  }
+  return false;
 }
+bool find_optimal(int n, vector<moves> & movements, long distance, long bonus, long timer){
+  vector < moves > s1;
+  int first_half = n/2;
+  int second_half = n - first_half;
+  long lim = ((long)1)<<(first_half);
+  for(long i=0; i < lim; i++){
+    long total_distance = 0;
+    long total_time =0;
+    for(long j=0; j < first_half; j++){
+      if((((long)1)<<j) & i){
+        total_distance += (movements[j].first + bonus);
+        total_time += movements[j].second;
+      }
+    }
+    s1.push_back(make_pair(total_distance, total_time));
+  }
+  sort(s1.begin(), s1.end(), [](moves a, moves b) {
+    if(a.first == b.first){
+      return a.second > b.second;
+    }
+      return a.first < b.first;   
+  });
+  long minimum = LONG_MAX;
+  for(int i=(int)s1.size()-1; i>=0; i--){
+    s1[i].second = min(minimum, s1[i].second);
+    minimum = s1[i].second;
+  }
+  lim = ((long)1)<<(second_half);
+  for(long i=0; i < lim; i++){
+    long total_distance = 0;
+    long total_time =0;
+    for(long j = first_half ; j < n; j++){
+      if(((long)1)<<j & (i<<(first_half))){
+        total_distance += movements[j].first + bonus;
+        total_time += movements[j].second;
+      }
+    }
+    if(binary_search(s1, distance-total_distance, timer-total_time))
+      return true;
+  }
+  return false;
+}
+
 void solve(){
-    int n, m;
-    long d,t;
-    std::cin>>n>>m>>d>>t;
-    vector<long> movements(n);
-    vector<long> elapsed(n);
-    
-    for(int i=0; i < n; i++){ 
-        std::cin>>movements[i]>>elapsed[i];
-    }
-    vector<long> boost(m+1);
-    boost[0]=0;
-    for(int i=1; i <= m; i++){
-        std::cin>>boost[i];
-    }
+  int n,m; long d,t; cin>>n>>m>>d>>t;
+  vector<moves> movements(n);
+  long a,b;
+  for(int i=0; i< n;i++){
+    cin>>a>>b; movements[i] = make_pair(a,b);
+  }
+  vector<long> gulps(m+1);
+  gulps[0] = 0;
+  for(int i=1; i<=m; i++){
+    cin>>gulps[i];
+  }
 
-
-    int res = findbest(movements, elapsed, boost, 0, m, d, t);
-    if(res == -1){
-        std::cout<<"Panoramix captured\n";
-        return;
-    } 
-    std::cout<<res<<"\n";
-}
-int findbest(const vector<long>& movements,const vector<long>& elapsed,const vector<long>& boost,const int left,const int right, const long & distance,const long & maxtime){
-    int res;
-    if(left>right){
-        return -1;
-    }
-    int center =(left+right)/2;
-    if(isdoable(movements, elapsed, boost[center], distance, maxtime)){
-        res = findbest(movements, elapsed, boost, left, center-1, distance, maxtime);
-        if(res != -1)
-            return res;
-        return center;
+  int res=-1;
+  int left = 0;
+  int right = m;
+  while(left <= right){
+    int mid = (left + right)/2;
+    if(find_optimal(n, movements, d, gulps[mid], t)){
+      res = mid;
+      right = mid-1;
     }
     else{
-        return findbest(movements, elapsed, boost, center+1, right, distance, maxtime);
+      left = mid+1;
     }
+  }
+  if(res == -1)
+    cout<<"Panoramix captured\n";
+  else
+    cout<<res<<"\n";
 }
-bool isdoable(const vector<long>& movements, const vector<long> & elapsed, long boost, const long & distance, const long & maxtime){
-    int size = movements.size();
-    int len = size/2;
-    int max1 = 1<<(size-len);
-    int max2 = 1<<len;
-    long sum;
-    long totaltime;
-    int tmp;
-    long tofind;
-    long min;
-    map <long, long > othersector;
-    for(int i=0; i < max1; i++){
-        sum =0;
-        totaltime=0;
-        for(int j = len; j < size; j++){
-            tmp = j-len;
-            if(i & 1<<tmp){
-                sum += movements[j] + boost;
-                totaltime += elapsed[j];
-            }
-        }
-        othersector.insert(std::make_pair(sum, totaltime));
-    }
-    min = LONG_MAX;
-    for(auto iter = othersector.rbegin(); iter != othersector.rend(); iter++){
-        if(iter->second < min){
-            min = iter->second;
-        }
-        else{
-            iter->second = min;
-        }
-    }
-    for(int i=0; i < max2; i++){
-        sum=0;
-        totaltime=0;
-        for(int j=0; j < len; j++){
-            if(i & 1<<j){
-                sum += movements[j] + boost;
-                totaltime += elapsed[j];
-            }
-        }
-        tofind = distance-sum;
-        auto finder = othersector.lower_bound(tofind);
-        if(finder != othersector.end() && finder->second + totaltime < maxtime){
-            return true;
-        }
-    }
-    return false;
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int t; cin>>t;
+  while(t--) solve();
 }
